a08a2a91cdb4d4dd2c187c03c9fef487dc88d1b4
Yomaira Escano <yjescano@gmail.com>
2018-10-03 05:28:14 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,41 +1,41 @@
 // your code here
 function displayCommits() {
-    let commits = JSON.parse(this.responseText)
-    let commitsList = `<ul>${commits.map(commit =>
-        '<li>' + commit.commit.author.name + ' - ' + commit.author.login + ' - ' + commit.commit.message + '</li>').join('')}</ul>`
+  let commits = JSON.parse(this.responseText)
+  let commitsList = `<ul>${commits.map(commit =>
+      '<li>' + commit.commit.author.name + ' - ' + commit.author.login + ' - ' + commit.commit.message + '</li>').join('')}</ul>`
 
-    document.getElementById('details').innerHTML = commitsList
+  document.getElementById('details').innerHTML = commitsList
 }
 
 function displayBranches() {
-    let branches = JSON.parse(this.responseText)
-    let branchesList = `<ul>${branches.map(branch =>
-        '<li>' + branch.name + '</li>').join('')}</ul>`
+  let branches = JSON.parse(this.responseText)
+  let branchesList = `<ul>${branches.map(branch =>
+      '<li>' + branch.name + '</li>').join('')}</ul>`
 
-    document.getElementById('details').innerHTML = branchesList
+  document.getElementById('details').innerHTML = branchesList
 }
 
 function displayRepositories() {
-    let repos = JSON.parse(this.responseText)
-    let reposList = `<ul>${repos.map(repo =>
-        '<li>' + repo.full_name + ' - ' + repo.html_url + '</li>').join('')}</ul>`
-    document.getElementById('repositories').innerHTML = reposList
+  let repos = JSON.parse(this.responseText)
+  let reposList = `<ul>${repos.map(repo =>
+      '<li>' + repo.full_name + ' - ' + repo.html_url + '</li>').join('')}</ul>`
+  document.getElementById('repositories').innerHTML = reposList
 }
 
 function getRepositories() {
-    let req = new XMLHttpRequest()
-    req.open('GET', 'https://api.github.com/users/octocat/repos')
-    req.send()
+  let req = new XMLHttpRequest()
+  req.open('GET', 'https://api.github.com/users/octocat/repos')
+  req.send()
 }
 
 function getCommits() {
-    let req = new XMLHttpRequest()
-    req.open('GET', 'https://api.github.com/repos/octocat/Spoon-Knife/commits')
-    req.send()
+  let req = new XMLHttpRequest()
+  req.open('GET', 'https://api.github.com/repos/octocat/Spoon-Knife/commits')
+  req.send()
 }
 
 function getBranches() {
-    let req = new XMLHttpRequest()
-    req.open('GET', 'https://api.github.com/repos/octocat/Spoon-Knife/branches')
-    req.send()
+  let req = new XMLHttpRequest()
+  req.open('GET', 'https://api.github.com/repos/octocat/Spoon-Knife/branches')
+  req.send()
 }
